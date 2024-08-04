971b650964242f532386789366a11de6ea980581
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-04 22:32:42 +0100

    write admin form controllers and view

---
+++ controllers/memberController.js
@@ -0,0 +1,38 @@
+function joinAdminGet(req, res, next) {
+  res.render("join-admin-form");
+}
+
+const joinAdminPost = [
+    body("adminPassword").notEmpty().withMessage("Try again...but write something!"),
+    async function (req, res, next) {
+      const err = validationResult(req);
+  
+      if (!err.isEmpty()) {
+        const error = err.array();
+        return res.render("join-admin-form", {
+          message: error[0].msg,
+        });
+      }
+  
+      const { rows } = await pool.query(
+        "SELECT * FROM passes WHERE pass_name = 'admin pass';",
+      );
+      const passHash = rows[0].pass_hash;
+  
+      const isMatch = await bcrypt.compare(req.body.adminPassword, passHash);
+      if (!isMatch) {
+        return res.render("join-admin-form", {
+          message: "No admin for you",
+        });
+      }
+  
+      const updateUser = await pool.query(
+        "UPDATE users SET admin = true WHERE id = $1 RETURNING *",
+        [req.user.id],
+      );
+      console.log(updateUser.rows[0]);
+      res.send("HONK HONK");
+    },
+  ];
+
+module.exports = { joinAdminGet, joinAdminPost };
