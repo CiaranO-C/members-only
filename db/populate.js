b0a2f313af421500fec0ebeb541ccb3b83b88a88
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-04 22:34:57 +0100

    write hashsecrets function, create new password table

--- db/populate.js
+++ db/populate.js
@@ -1,13 +1,21 @@
-//require("dotenv").config();
+require("dotenv").config();
 const pg = require("pg");
-
+const bcrypt = require("bcryptjs");
 const client = new pg.Client({
   connectionString: process.env.CONNECTION_STRING,
 });
 
+async function hashSecrets() {
+  const memberHash = await bcrypt.hash(process.env.MEMBER_PASS, 10);
+  const adminHash = await bcrypt.hash(process.env.ADMIN_PASS, 12);
 
-async function genTables(){
-const sql = `
+  const sql = `INSERT INTO passes VALUES('member pass', '${memberHash}', 10), ('admin pass', '${adminHash}', 12);`;
+
+  await client.query(sql);
+}
+
+async function genTables() {
+  const sql = `
 CREATE TABLE IF NOT EXISTS users(
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 first_name VARCHAR(255) NOT NULL,
@@ -15,7 +23,8 @@ last_name VARCHAR(255) NOT NULL,
 email VARCHAR(320) UNIQUE NOT NULL,
 password_hash VARCHAR(255) NOT NULL,
 salt VARCHAR(255) NOT NULL,
-member BOOLEAN DEFAULT false
+member BOOLEAN DEFAULT false,
+admin BOOLEAN DEFAULT false
 );
 
 CREATE TABLE IF NOT EXISTS messages(
@@ -26,14 +35,20 @@ text TEXT NOT NULL,
 sent_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY (user_id) REFERENCES users(id)
 );
-`
 
-await client.connect();
-await client.query(sql);
-await client.end();
+CREATE TABLE IF NOT EXISTS passes(
+pass_name VARCHAR(255) PRIMARY KEY NOT NULL,
+pass_hash VARCHAR(255) UNIQUE NOT NULL,
+salt INTEGER NOT NULL
+);
+`;
 
-console.log('Tables created')
+  await client.connect();
+  await client.query(sql);
+  await hashSecrets();
+  await client.end();
+
+  console.log("Tables created");
 }
 
 genTables();
-
